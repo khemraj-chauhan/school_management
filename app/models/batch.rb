d7b940c0327643d6465a0a0621fa25e97c0fbe9f
Khemraj Chauhan <khemrajchauhan.kr@gmail.com>
2023-07-09 17:23:48 +0530

    adding associationos for course and student batches

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,5 +1,8 @@
 class Batch < ApplicationRecord
   validates :name, :description, presence: true
 
-  belongs_to :batch
+  belongs_to :course
+
+  has_many :student_batches, dependent: :destroy
+  has_many :students, through: :student_batches
 end
