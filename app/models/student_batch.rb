fb296a8bd9519340a1f48689348a3222da9bca95
Khemraj Chauhan <khemrajchauhan.kr@gmail.com>
2023-07-09 17:24:45 +0530

    added student batch associations and status enum

--- app/models/student_batch.rb
+++ app/models/student_batch.rb
@@ -1,2 +1,6 @@
 class StudentBatch < ApplicationRecord
+  belongs_to :student, class_name: "User", foreign_key: "student_id"
+  belongs_to :batch
+
+  enum status: { pending: "pending", approved: "approved", rejected: "rejected" }, _default: "pending"
 end
